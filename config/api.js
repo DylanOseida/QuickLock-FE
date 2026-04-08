062c652cc7b467648aa8f74c3007577b2843a42e
DylanOseida <dylan.oseida.103@my.csun.edu>
2026-04-07 22:22:58 -0700

    logout functionality, dynamic devices page, and dynamic home page with user-specific data.

--- config/api.js
+++ config/api.js
@@ -198,6 +198,27 @@ export async function fetchLocks() {
   }
 }
 
+export async function fetchAdminLocks() {
+  const token = await getAccessToken();
+  if (!token) throw new Error("No access token");
+
+  try {
+    const res = await axios.get(`${BASE_URL}/access/Locks/`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return res.data;
+  } catch (err) {
+    const status = err.response?.status;
+    const data = err.response?.data;
+
+    console.error("Failed to fetch admin locks:", status, data || err.message);
+    throw err;
+  }
+}
+
 export async function saveLockId(lockId) {
   // Treat null/undefined/"" as "clear"
   if (lockId === null || lockId === undefined || String(lockId).trim() === "") {
@@ -303,4 +324,12 @@ export async function generateKey(data) {
   return payload;
 }
 
-
+export async function logoutUser() {
+  try {
+    await removeTokens();
+    await removeLockId();
+    console.log("user logged out")
+  } catch (err) {
+    console.error("Logout failed:", err);
+  }
+}
